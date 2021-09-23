cf1967633da9f513c8474a4ec0e8e1765cd0bc6f
GK Thakur <t.gov.92@gmail.com>
2021-09-23 22:53:45 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./app.module.css";
+import { fetchData } from "./api";
+import coronaImages from "./Images/image_covid.png";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    //fetch the country data
+    const fetchedData = await fetchData(country);
+    // set the state of country data
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.images} src={coronaImages} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
